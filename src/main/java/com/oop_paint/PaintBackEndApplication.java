d06cce27a577bac77fa8353a1660c734cb70e359
josephShokry <96320048+josephShokry@users.noreply.github.com>
2022-11-30 18:26:00 +0200

    initialize the project

---
+++ src/main/java/com/oop_paint/PaintBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.oop_paint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oop_paint/PaintBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop_paint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
